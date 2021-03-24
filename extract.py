1ec063e7f02893ffbd348147c114edcc5b9505a0
jiazhifeng <jiazhifeng@sensetime.com>
2021-03-24 12:22:21 +0800

    Update

--- extract.py
+++ extract.py
@@ -18,3 +18,14 @@ for each_line in lines:
         output.append(int(li[0]))
 
 print(list(set(output)))
+
+output_2 = []
+for each_line in lines:
+    li = each_line.split('\t')
+    # print(int(li[0]))
+    if int(li[0]) in output and int(li[2]) not in add and int(li[2]) not in key:
+        output_2.append(int(li[2]))
+    if int(li[2]) in output and int(li[0]) not in add and int(li[0]) not in key:
+        output_2.append(int(li[0]))
+
+print(list(set(output_2)))
